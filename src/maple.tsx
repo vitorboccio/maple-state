d90d5a54d8d495dd956aabb8d555d84725d4ab17
Vitor Boccio <vitorboccio@gmail.com>
2023-03-24 19:40:39 -0700

    maple function

--- src/maple.tsx
+++ src/maple.tsx
@@ -1,1 +1,38 @@
 
+type Maple<MapleType> = {
+  get: () => MapleType
+  set: (newValue: MapleType) => void
+  subscribe: (callback: (newValue: MapleType) => void) => () => void
+  _subscribers: () => number
+}
+
+type MapleGetter<MapleType> = (
+  get: <Target>(m: Maple<Target>) => Target,
+) => MapleType
+
+export function maple<MapleType>(
+  initialValue: MapleType | MapleGetter<MapleType>,
+) : Maple<MapleType> {
+  let value: MapleType = typeof initialValue === "function" ? (null as MapleType) : initialValue
+  
+  const subscribers = new Set<(newValue: MapleType) => void>()
+
+  return {
+    get() {
+      return value
+    },
+    set(newValue) {
+      value = newValue
+      subscribers.forEach((callback) => callback(value))
+    },
+    subscribe(callback) {
+      subscribers.add(callback)
+      return () => {
+        subscribers.delete(callback)
+      }
+    },
+    _subscribers() {
+      return subscribers.size
+    },
+  }
+}
